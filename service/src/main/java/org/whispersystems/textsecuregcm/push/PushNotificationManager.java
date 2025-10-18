661f9786637c15cf360cc0c521d227b6c5db8d88
yangweiguang <yangwg0809@163.com>
2025-10-18 23:58:30 +0800

    send voip

--- service/src/main/java/org/whispersystems/textsecuregcm/push/PushNotificationManager.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/push/PushNotificationManager.java
@@ -102,13 +102,8 @@ public class PushNotificationManager {
 
     if (StringUtils.isNotBlank(device.getGcmId())) {
       tokenAndType = new Pair<>(device.getGcmId(), PushNotification.TokenType.FCM);
-      logger.info("[voip] tokenType:{}, token:{}", tokenAndType.first(), tokenAndType.second());
-    } else if (StringUtils.isNotBlank(device.getVoipApnId())) {
-      tokenAndType = new Pair<>(device.getVoipApnId(), PushNotification.TokenType.VOIP_APN);
-      logger.info("[voip] tokenType:{}, token:{}", tokenAndType.first(), tokenAndType.second());
     } else if (StringUtils.isNotBlank(device.getApnId())) {
       tokenAndType = new Pair<>(device.getApnId(), PushNotification.TokenType.APN);
-      logger.info("[voip] tokenType:{}, token:{}", tokenAndType.first(), tokenAndType.second());
     } else {
       throw new NotPushRegisteredException();
     }
